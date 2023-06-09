5642402bf653394eb36ffbcff2fd7a5a2b020ce6
SidDaSleuth <64015559+onlywoo@users.noreply.github.com>
2023-06-09 10:49:57 +0200

    doijfi

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Router, HashRouter as router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
@@ -9,9 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Router>
-        <App />
-      </Router>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
