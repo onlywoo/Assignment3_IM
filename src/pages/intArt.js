cb8bee2f42a189bd90bee6ed7b4848e72abf26cc
SidDaSleuth <64015559+onlywoo@users.noreply.github.com>
2023-06-25 20:00:28 +0200

    idk

--- src/pages/intArt.js
+++ src/pages/intArt.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import "../styles/intArt.css";
 import Popup from "../components/Popup";
-export default class intArt extends Component {
-  render() {
-    return;
-    <main>
-      <button>Open the Game</button>;<Popup trigger={false}>My popup</Popup>;
-    </main>;
+function pop() {
+  {
+    return (
+      <div className="pop">
+        <main>
+          <button>Open the Game</button>
+          <Popup trigger={false}>
+            {" "}
+            <h1>My popup</h1>
+          </Popup>
+        </main>
+      </div>
+    );
   }
 }
+export default pop;
