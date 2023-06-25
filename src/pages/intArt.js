3985c9d5de0e22147dab119b3278de214d6bfe7e
SidDaSleuth <64015559+onlywoo@users.noreply.github.com>
2023-06-25 20:16:17 +0200

    man

--- src/pages/intArt.js
+++ src/pages/intArt.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import "../styles/intArt.css";
 import Popup from "../components/Popup";
+import { useState } from "react";
 function pop() {
-  {
-    return (
-      <div className="pop">
-        <main>
-          <button>Open the Game</button>
-          <Popup trigger={false}>
-            {" "}
-            <h1>My popup</h1>
-          </Popup>
-        </main>
-      </div>
-    );
-  }
+  const [buttonPopup, setButtonPopup] = useState(false);
+
+  return (
+    <div className="pop">
+      <main>
+        <button onClick={() => setButtonPopup(true)}>Open the Game</button>
+      </main>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h1>My popup</h1>
+      </Popup>
+    </div>
+  );
 }
 export default pop;
