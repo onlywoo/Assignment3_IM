5101f761d8c996274697df6e7ee0a26019690827
SidDaSleuth <64015559+onlywoo@users.noreply.github.com>
2023-06-26 00:40:32 +0200

    idk

--- src/helpers/PopupList.js
+++ src/helpers/PopupList.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "../styles/intArt.css";
-import firstAd from "..assets/5.png";
-import secAd from "..assets/6.png";
-import thirdAd from "..assets/7.png";
-import fourthAd from "..assets/8.png";
-import fifthAd from "..assets/9.png";
-import sixthAd from "..assets/10.png";
-import seventhAd from "..assets/11.png";
-import eigthAd from "..assets/12.png";
-import nineAd from "..assets/13.png";
-import tenAd from "..assets/14.png";
-import elevenAd from "..assets/15.png";
-import twelveAd from "..assets/16.png";
+import img from "";
+import firstAd from "..src/assets/5.png";
+import secAd from "..src/assets/6.png";
+import thirdAd from "..src/assets/7.png";
+import fourthAd from "..src/assets/8.png";
+import fifthAd from "..src/assets/9.png";
+import sixthAd from "..src/assets/10.png";
+import seventhAd from "..src/assets/11.png";
+import eigthAd from "..src/assets/12.png";
+import nineAd from "..src/assets/13.png";
+import tenAd from "..src/assets/14.png";
+import elevenAd from "..src/assets/15.png";
+import twelveAd from "..src/assets/16.png";
 
 export const PopupList = [
   {
