f13ea5e900f71d634f3eaf5ab6ca477266e4e2c5
SidDaSleuth <64015559+onlywoo@users.noreply.github.com>
2023-06-26 00:45:17 +0200

    imagse

--- src/helpers/PopupList.js
+++ src/helpers/PopupList.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "../styles/intArt.css";
-import firstAd from "..assets/5.png";
-import secAd from "..assets/6.png";
-import thirdAd from "..assets/7.png";
-import fourthAd from "..assets/8.png";
-import fifthAd from "..assets/9.png";
-import sixthAd from "..assets/10.png";
-import seventhAd from "..assets/11.png";
-import eigthAd from "..assets/12.png";
-import nineAd from "..assets/13.png";
-import tenAd from "..assets/14.png";
-import elevenAd from "..assets/15.png";
-import twelveAd from "..assets/16.png";
+import firstAd from "../assets/5.png";
+import secAd from "../assets/6.png";
+import thirdAd from "../assets/7.png";
+import fourthAd from "../assets/8.png";
+import fifthAd from "../assets/9.png";
+import sixthAd from "../assets/10.png";
+import seventhAd from "../assets/11.png";
+import eigthAd from "../assets/12.png";
+import nineAd from "../assets/13.png";
+import tenAd from "../assets/14.png";
+import elevenAd from "../assets/15.png";
+import twelveAd from "../assets/16.png";
 
 export const PopupList = [
   {
