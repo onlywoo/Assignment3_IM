2aef15b9b65633a6a22790858a7b8abae5d47e66
SidDaSleuth <64015559+onlywoo@users.noreply.github.com>
2023-06-26 07:43:07 +0200

    popup

--- src/pages/intArt.js
+++ src/pages/intArt.js
@@ -3,12 +3,27 @@ import { PopupList } from "../helpers/PopupList";
 import PopupItems from "../components/PopupItems";
 import "../styles/intArt.css";
 import Play from "../assets/playButton.png";
+import firstAd from "../assets/5.png";
+import secAd from "../assets/6.png";
+import thirdAd from "../assets/7.png";
+import fourthAd from "../assets/8.png";
+import fifthAd from "../assets/9.png";
+import sixthAd from "../assets/10.png";
+import seventhAd from "../assets/11.png";
+import eigthAd from "../assets/12.png";
+import nineAd from "../assets/13.png";
+import tenAd from "../assets/14.png";
+import elevenAd from "../assets/15.png";
+import twelveAd from "../assets/16.png";
 
 export default class popupAds extends Component {
   render() {
     return (
       <div className="menu">
         <h1 className="menuTitle">
+          <div class="grid-container">
+            <img src={firstAd} />
+          </div>
           <img src={Play} />
         </h1>
         <div>
